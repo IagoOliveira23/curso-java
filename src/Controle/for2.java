9fe978dbb6577775430f7ef514b7a1a0e3961615
IagoOliveira23 <iago.ishift@gmail.com>
2023-06-09 17:01:53 -0300

    for3

---
+++ src/Controle/for2.java
@@ -0,0 +1,9 @@
+package Controle;
+
+public class for2 {
+    public static void main(String[] args) {
+        for(int a = 10; a >= 0; a-=2){
+            System.out.printf("a = %d\n", a);
+        }
+    }
+}
