33bf828a60177f4f8283ca091828516b99980912
IagoOliveira23 <iago.ishift@gmail.com>
2023-06-10 09:12:37 -0300

    Switch sem break

---
+++ src/Controle/SwitchSemBreak.java
@@ -0,0 +1,24 @@
+package Controle;
+
+public class SwitchSemBreak {
+    public static void main(String[] args) {
+        
+        String faixa = "preta";
+        switch(faixa.toLowerCase()){
+        case "preta":
+            System.out.println("Sei o Bassai-Dai...");
+        case "marron":
+            System.out.println("Sei o Tekki Shodan");
+        case "rosa":
+            System.out.println("Sei o Heian Godan");
+        case "verde":
+            System.out.println("Sei o Heian Yodan");
+        case "laranja":
+            System.out.println("Sei o Heian Sandan");
+        case "vermelha":
+            System.out.println("Seu o Heian Nidan");
+        case "amarela":
+            System.out.println("Sei o Heian Shodan");
+        }
+    }
+}
