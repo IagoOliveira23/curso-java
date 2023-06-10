6833266f6cb3942090e7850ba2aa88d7b5ff7b33
IagoOliveira23 <iago.ishift@gmail.com>
2023-06-10 08:59:38 -0300

    desafio for

---
+++ src/Controle/DesafioFor.java
@@ -0,0 +1,19 @@
+package Controle;
+
+public class DesafioFor {
+    public static void main(String[] args) {
+        String valor = "#";
+        for(int i = 1; i <= 5; i++){
+            System.out.println(valor);
+            valor += "#";
+        }
+
+        // Versão do desafio
+        // Não pode usar valor numérico para controlar o laço!
+
+        String valor2 = "#";
+        for(valor2.equals("#"); !valor2.equals("######"); valor2+="#"){
+            System.out.println(valor2);
+        }
+    }
+}
