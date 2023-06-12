34a29c03f21d39c3f002b49a4cf440109e8b03ac
IagoOliveira23 <iago.ishift@gmail.com>
2023-06-12 08:03:24 -0300

    classe produto

---
+++ src/classe/ProdutoTeste.java
@@ -0,0 +1,18 @@
+package classe;
+
+public class ProdutoTeste {
+    
+    public static void main(String[] args) {
+        Produto p1 = new Produto();
+        p1.nome = "Notebook";
+        p1.preco = 4356.89;
+        p1.desconto = 0.25;
+
+        System.out.println("Nome: " + p1.nome);
+        System.out.println("Preço: " + p1.preco);
+        System.out.println("Desconto: " + p1.desconto);
+
+        double precoFinal = p1.preco * (1 - p1.desconto);
+        System.out.printf("Preço final = R$%.2f", precoFinal);
+    }
+}
