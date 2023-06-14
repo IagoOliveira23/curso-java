a4204d14c70dd6edd7101a8db2f26eaa38f861a6
IagoOliveira23 <iago.ishift@gmail.com>
2023-06-14 19:31:48 -0300

    adc método para retornar data formatada

--- src/classe/Data.java
+++ src/classe/Data.java
@@ -4,4 +4,8 @@ public class Data {
     int dia;
     int mes;
     int ano;
+
+    String retornarData(){
+        return String.format("%d/%d/%d", dia, mes, ano); 
+    }
 }
--- src/classe/DataTeste.java
+++ src/classe/DataTeste.java
@@ -7,14 +7,14 @@ public class DataTeste {
         d1.mes = 6;
         d1.ano = 2023;
 
-        System.out.printf("%d/%d/%d\n", d1.dia, d1.mes, d1.ano);
+        System.out.println(d1.retornarData());
         
         Data d2 = new Data();
         d2.dia = 11;
         d2.mes = 6;
         d2.ano = 2023;
 
-        System.out.printf("%d/%d/%d\n", d2.dia, d2.mes, d2.ano);
+        System.out.println(d2.retornarData());
         
     }
 }
