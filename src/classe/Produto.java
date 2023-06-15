8928d7a52f2442f1ba9698454de8d990d7b892dc
IagoOliveira23 <iago.ishift@gmail.com>
2023-06-15 10:13:00 -0300

    adc construtores

--- src/classe/Produto.java
+++ src/classe/Produto.java
@@ -5,6 +5,20 @@ public class Produto {
     String nome;
     double preco;
     double desconto;
+    
+    Produto(){
+
+    }
+
+    Produto(String nomeInicial){
+        nome = nomeInicial;
+    }
+
+    Produto(String nomeInicial, double precoInicial, double descInicial){
+        nome = nomeInicial;
+        preco = precoInicial;
+        desconto = descInicial;
+    }
 
     double desconto(){
         return preco * (1 - desconto);
--- src/classe/ProdutoTeste.java
+++ src/classe/ProdutoTeste.java
@@ -12,7 +12,26 @@ public class ProdutoTeste {
         System.out.println("Preço: " + p1.preco);
         System.out.println("Desconto: " + p1.desconto);
 
-        double precoFinal = p1.desconto();
-        System.out.printf("Preço final = R$%.2f", precoFinal);
+        double precoFinalN = p1.desconto();
+        System.out.printf("Preço final = R$%.2f\n", precoFinalN);
+
+        Produto p2 = new Produto("Celular");
+        p2.preco = 2599.99;
+        p2.desconto = 0.30;
+
+        System.out.println("\nNome: " + p2.nome);
+        System.out.println("Preço: " + p2.preco);
+        System.out.println("Desconto: " + p2.desconto);
+
+        double precoFinalC = p2.desconto();
+        System.out.printf("Preço final = R$%.2f\n", precoFinalC);
+
+        Produto p3 = new Produto("Teclado", 250, 0.05);
+        System.out.println("\nNome: " + p3.nome);
+        System.out.println("Preço: " + p3.preco);
+        System.out.println("Desconto: " + p3.desconto);
+
+        double precoFinalT = p3.desconto();
+        System.out.printf("Preço final = R$%.2f\n", precoFinalT);
     }
 }
