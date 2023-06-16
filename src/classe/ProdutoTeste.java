a0be8f880330a4d5c6c9faa65b6448add610d043
IagoOliveira23 <iago.ishift@gmail.com>
2023-06-16 08:25:03 -0300

    método static

--- src/classe/ProdutoTeste.java
+++ src/classe/ProdutoTeste.java
@@ -6,30 +6,30 @@ public class ProdutoTeste {
         Produto p1 = new Produto();
         p1.nome = "Notebook";
         p1.preco = 4356.89;
-        p1.desconto = 0.25;
+        Produto.desconto = 0.15;
 
         System.out.println("Nome: " + p1.nome);
         System.out.println("Preço: " + p1.preco);
-        System.out.println("Desconto: " + p1.desconto);
+        System.out.println("Desconto: " + Produto.desconto);
 
         double precoFinalN = p1.desconto();
         System.out.printf("Preço final = R$%.2f\n", precoFinalN);
 
         Produto p2 = new Produto("Celular");
         p2.preco = 2599.99;
-        p2.desconto = 0.30;
+        Produto.desconto = 0.30;
 
         System.out.println("\nNome: " + p2.nome);
         System.out.println("Preço: " + p2.preco);
-        System.out.println("Desconto: " + p2.desconto);
+        System.out.println("Desconto: " + Produto.desconto);
 
         double precoFinalC = p2.desconto();
         System.out.printf("Preço final = R$%.2f\n", precoFinalC);
 
-        Produto p3 = new Produto("Teclado", 250, 0.05);
+        Produto p3 = new Produto("Teclado", 250);
         System.out.println("\nNome: " + p3.nome);
         System.out.println("Preço: " + p3.preco);
-        System.out.println("Desconto: " + p3.desconto);
+        System.out.println("Desconto: " + Produto.desconto);
 
         double precoFinalT = p3.desconto();
         System.out.printf("Preço final = R$%.2f\n", precoFinalT);
