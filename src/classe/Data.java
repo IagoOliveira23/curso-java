eb20055d8f9fdb099811eab75a7a09bcc8e5f47a
IagoOliveira23 <iago.ishift@gmail.com>
2023-06-17 12:57:40 -0300

    adc this e this()

--- src/classe/Data.java
+++ src/classe/Data.java
@@ -6,15 +6,13 @@ public class Data {
     int ano;
 
     Data(){
-        dia = 1;
-        mes = 1;
-        ano = 1970;
+        this(1, 1, 1970);
     }
 
-    Data(int d, int m, int a){
-        dia = d;
-        mes = m;
-        ano = a;
+    Data(int dia, int mes, int ano){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
     }
     String retornarData(){
         return String.format("%d/%d/%d", dia, mes, ano); 
