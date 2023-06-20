efe97950e4c5f0a01db3c59a7d9a1bcc76ca621f
IagoOliveira23 <iago.ishift@gmail.com>
2023-06-20 10:40:45 -0300

    Foreach

---
+++ src/arrays/Foreach.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class Foreach {
+    
+    public static void main(String[] args) {
+        double[] notas = {8.4, 5.5, 7.5, 10};
+
+        for(double nota: notas){
+        System.out.println(nota);
+        }
+    }
+}
