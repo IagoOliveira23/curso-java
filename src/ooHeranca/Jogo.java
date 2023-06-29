d734ffdddff1f759ec2591f55334e831059c2df9
IagoOliveira23 <iago.ishift@gmail.com>
2023-06-29 16:14:39 -0300

    add atacar

--- src/ooHeranca/Jogo.java
+++ src/ooHeranca/Jogo.java
@@ -3,16 +3,21 @@ package ooHeranca;
 public class Jogo {
     public static void main(String[] args) {
         Jogador j1 = new Jogador();
-        j1.x = 0;
-        j1.y = 0;
+        j1.x = 10;
+        j1.y = 10;
 
-        j1.andar(Direcao.NORTE);
-        j1.andar(Direcao.NORTE);
-        j1.andar(Direcao.NORTE);
-        j1.andar(Direcao.SUL);
-        j1.andar(Direcao.OESTE);
+        Jogador j2 = new Jogador();
+        j2.x = 10;
+        j2.y = 10;
 
-        System.out.println(j1.y);
-        System.out.println(j1.x);
+
+        System.out.println(j1.vida);
+        System.out.println(j2.vida);
+
+        j1.atacar(j2);
+        j2.atacar(j1);
+
+        System.out.println(j1.vida);
+        System.out.println(j2.vida);
     }
 }
