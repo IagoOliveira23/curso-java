9afe16fb7357fbd0a17c4ef8847d595f6fa12681
IagoOliveira23 <iago.ishift@gmail.com>
2023-06-30 16:08:48 -0300

    add SuppressWarnings

--- src/oo/encapsulamento/casaA/Ana.java
+++ src/oo/encapsulamento/casaA/Ana.java
@@ -1,6 +1,7 @@
 package oo.encapsulamento.casaA;
 
 public class Ana {
+    @SuppressWarnings("unused")
     private String segredo = "...";
     String facoDentroDeCasa = "..."; // default/pacote
     protected String formaDeFalar = "...";
