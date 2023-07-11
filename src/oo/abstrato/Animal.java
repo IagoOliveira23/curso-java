6070a535b5d4cf0a9bc03025342217fb7ff2b47b
IagoOliveira23 <iago.ishift@gmail.com>
2023-07-11 17:30:23 -0300

    classe abstrata

---
+++ src/oo/abstrato/Animal.java
@@ -0,0 +1,10 @@
+package oo.abstrato;
+
+public abstract class Animal {
+
+    public String respirar(){
+        return "Respirando...";
+    }
+
+    public abstract String mover();
+}
---
+++ src/oo/abstrato/Cachorro.java
@@ -0,0 +1,16 @@
+package oo.abstrato;
+
+public class Cachorro extends Mamifero{
+
+    @Override
+    public String mamar() {
+        return "mamando...";
+    }
+
+    @Override
+    public String mover() {
+        return "locomovendo...";
+    }
+    
+    
+}
