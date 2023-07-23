a450cff5791bb2abb5a85cc84d9dc300c531c48e
IagoOliveira23 <iago.ishift@gmail.com>
2023-07-22 22:20:27 -0300

    Reduce exemplo 2

--- src/streams/Aluno.java
+++ src/streams/Aluno.java
@@ -3,11 +3,17 @@ package streams;
 public class Aluno {
     final String nome;
     final double nota;
+    final boolean bomComportamento;
 
     
-    public Aluno(String nome, double nota) {
+    public Aluno(String nome, double nota, boolean bomComportamento) {
         this.nome = nome;
         this.nota = nota;
+        this.bomComportamento = bomComportamento;
+    }
+
+    public Aluno(String nome, double nota){
+        this(nome, nota, true);
     }
 
     
