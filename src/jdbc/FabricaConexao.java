9f3510428ad17d1f1782164fbab91ea097fe46e5
IagoOliveira23 <iago.ishift@gmail.com>
2023-08-11 18:12:52 -0300

    abrindo conexão

--- src/jdbc/FabricaConexao.java
+++ src/jdbc/FabricaConexao.java
@@ -9,8 +9,8 @@ public class FabricaConexao{
     public static Connection getConexao(){
         try {
             final String url = "jdbc:mysql://localhost/curso_java?verifyCertificate=false&useSSL=true";
-            final String usuario = "user";
-            final String senha = "senha";
+            final String usuario = "iago";
+            final String senha = "1234";
 
             return DriverManager.getConnection(url, usuario, senha);
         }catch (SQLException e){
